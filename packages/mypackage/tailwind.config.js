b2348d14ada98f6a9f573c608b19978c6e53ddae
sourey <sitaulasaurav@gmail.com>
2024-01-19 22:34:10 +0545

    added shadcn/ui and configfured files for tailwind

--- packages/mypackage/tailwind.config.js
+++ packages/mypackage/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // for React
+  ],
   theme: {
-    extend: {},
+    // ...
   },
   plugins: [],
-}
-
+};
